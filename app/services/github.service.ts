e339183ab6d3412d776f465fed524463b9d414ea
mbowen13 <mbowen@utexas.edu>
2016-11-07 19:48:25 -0500

    Create github service

---
+++ app/services/github.service.ts
@@ -0,0 +1,22 @@
+import { Injectable }  from '@angular/core';
+import { Http, Headers }  from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class GithubService {
+  
+  private username: string;
+  private client_id = '0785e79404ae667d35f1';
+  private client_secret = 'b27af0b22ef4bf55e8f75fd114e5609b83975dcf';
+  
+  constructor(private _http: Http) {
+    console.log('GHService ready');
+    this.username = 'Mike B';
+  }
+  
+  getUser() {
+    var URL = 'http://api.github.com' + this.username + '?client_id=' + this.client_id + '&client_secret=' + this.client_secret;
+    return this._http.get(URL)
+    .map(res => res.json());
+  }
+}
