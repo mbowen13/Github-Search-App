b5bf187610ea29b0b7e47e6251f2fdc32082e033
mbowen13 <mbowen@utexas.edu>
2017-01-17 18:51:53 -0600

    fix: Resolved all errors and ready to deploy

--- app/components/profile.component.ts
+++ app/components/profile.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { GithubService } from '../services/github.service';
 
 @Component({
-    moduleId: module.id,
     selector: 'profile',
     templateUrl: 'profile.component.html'
 })
